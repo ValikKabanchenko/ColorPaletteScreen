84ef28f794f35730c0aed9be368a7ff662d6afd3
ValikKabanchenko <kabanchenkoval@gmail.com>
2022-06-08 02:28:21 +0300

    Base logic

--- ColorPaletteScreen/ViewController.swift
+++ ColorPaletteScreen/ViewController.swift
@@ -9,6 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var currentlySelectedValueRed: UILabel!
+    
+    @IBOutlet weak var currentlySelectedValueGreen: UILabel!
+    
+    @IBOutlet weak var currentlySelectedValueBlue: UILabel!
+    
+    @IBOutlet weak var colorMixed: UIView!
+    
+    
+    @IBAction func allSliderMin(_ sender: Any) {
+    }
+    
+    @IBAction func allSliderMedium(_ sender: Any) {
+    }
+    
+    @IBAction func allSliderMax(_ sender: Any) {
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
